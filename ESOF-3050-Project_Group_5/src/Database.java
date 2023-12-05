db461b235c37b82d7eb558c6ebda461071e204ac
david <ebubeorlukwu@gmail.com>
2023-12-05 13:19:04 -0500

    changes made to database.java

--- ESOF-3050-Project_Group_5/src/Database.java
+++ ESOF-3050-Project_Group_5/src/Database.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class Database {
 	//connection to MySQL using JDBC
     private static final String URL = "jdbc:mysql://localhost:3306/smartHome";
-    private static final String USERNAME = "user1";
-    private static final String PASSWORD = "root";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
     
 //establishes a connection to the MySQL database and returns the Connection
     public static Connection getConnection() throws SQLException {
